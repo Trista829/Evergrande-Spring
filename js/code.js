57135eb9eedf6fcff6a95854bb06d16c35899dff
unknown <591512134@qq.com>
2019-06-04 09:37:55 +0800

    water

---
+++ js/code.js
@@ -0,0 +1,8 @@
+document.getElementById("menu1").onclick = function (){
+	document.getElementById("box1").style.display = "block";
+	document.getElementById("box2").style.display = "none";
+}
+document.getElementById("menu2").onclick = function (){
+	document.getElementById("box2").style.display = "block";
+	document.getElementById("box1").style.display = "none";
+}
